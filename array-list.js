4d542c8b4e210581192838e53066ee10e616a70f
Peter Prakobkit <kprakobkit@gmail.com>
2015-01-10 00:32:01 -0800

    complete implement array list object

--- array-list.js
+++ array-list.js
@@ -18,10 +18,32 @@ ArrayList.prototype.get = function(index) {
 
 ArrayList.prototype.doubleSize = function() {
   var oldList = this.array;
-  this.array = new FixedArray(this.size * 2);
+  var newSize = this.size * 2;
+  this.size = newSize;
+  var newArray = new FixedArray(newSize);
+
   oldList.array.forEach(function(element, index) {
-    this.array.set(index, element);
+    newArray.set(index, element);
   });
+
+  this.array = newArray;
+};
+
+ArrayList.prototype.set = function(index, element) {
+    while(index > this.size - 1) this.doubleSize();
+    this.array.set(index, element);
+    return element;
+};
+
+ArrayList.prototype.insert = function(index, element) {
+  var oldElement = this.get(index);
+  this.set(index, element);
+
+  if (index == this.size - 1) {
+    this.add(oldElement);
+  } else {
+    this.insert(index+1,oldElement);
+  }
 };
 
 module.exports = ArrayList;
--- spec/array-list-spec.js
+++ spec/array-list-spec.js
@@ -1,21 +1,93 @@
 var ArrayList = require('../array-list.js');
 
 describe("Array List", function() {
-  var size = 5;
-  var arrayList = new ArrayList(size);
-
   describe("new", function() {
+    var size = 5;
+    var arrayList = new ArrayList(size);
+
     it("should create an array list object with the specified size", function() {
       expect(arrayList.size).toEqual(size);
     });
   });
 
   describe("add", function() {
+    var size = 5;
+    var arrayList = new ArrayList(size);
     var returnedElement = arrayList.add('element');
 
     it("should add an element at the end of the list", function() {
       expect(returnedElement).toEqual('element');
       expect(arrayList.get(size)).toEqual('element');
+    });
+  });
+
+  describe("get", function() {
+    var size = 5;
+    var arrayList = new ArrayList(size);
+    var returnedElement = arrayList.add('element');
+
+    it("should return the element at the specified index", function() {
+      expect(arrayList.get(size)).toEqual('element');
+    });
+  });
+
+  describe("set", function() {
+    var size = 5;
+    var arrayList = new ArrayList(size);
+    var element = 'element';
+    var newElement = 'new element';
+    var index = 4;
+    var returnedElement = arrayList.set(index, element);
+
+    it("should return the element", function() {
+      expect(returnedElement).toEqual(element);
+    });
+
+    it("should set the element at the specified index", function() {
+      expect(arrayList.get(index)).toEqual(element);
+    });
+
+    it("should replace the element at the specified index", function() {
+      arrayList.set(index, newElement);
+      expect(arrayList.get(index)).toEqual(newElement);
+    });
+
+    it("should not throw an error for a very large index ", function() {
+      var largeIndex = 100;
+      var largeElement = 1000;
+
+      arrayList.set(largeIndex, largeElement);
+      expect(arrayList.get(largeIndex)).toEqual(largeElement);
+    });
+  });
+
+  describe("insert", function() {
+    var size = 5;
+    var arrayList = new ArrayList(size);
+    var elements = ['first', 'second', 'third', 'fourth', 'fifth'];
+    var insertedElement = 'insert';
+    var middleIndex = 2;
+    elements.forEach(function(element, index) {
+      arrayList.set(index, element);
+    });
+
+    it("should insert an element at the middle of the list", function() {
+      arrayList.insert(middleIndex, insertedElement);
+      expect(arrayList.get(middleIndex)).toEqual(insertedElement);
+      expect(arrayList.get(middleIndex+1)).toEqual('third');
+      expect(arrayList.get(middleIndex-1)).toEqual('second');
+    });
+
+    it("should insert an element at the front of the list", function() {
+      arrayList.insert(0, insertedElement);
+      expect(arrayList.get(0)).toEqual(insertedElement);
+      expect(arrayList.get(1)).toEqual('first');
+    });
+
+    it("should insert an element at the end of the list", function() {
+      var lastIndex = arrayList.size-1;
+      arrayList.insert(lastIndex, insertedElement);
+      expect(arrayList.get(lastIndex)).toEqual(insertedElement);
     });
   });
 });
